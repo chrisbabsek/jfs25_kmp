95276c64851b37282a5fd5705cc73e1658239a39
Christian Babsek <christian.babsek@syna.de>
2025-07-08 21:56:29 +0200

    feat: minor improvements

--- shared/src/nativeMain/kotlin/de/babsek/jfs25kmp/NativeMain.kt
+++ shared/src/nativeMain/kotlin/de/babsek/jfs25kmp/NativeMain.kt
@@ -1,9 +1,6 @@
 package de.babsek.jfs25kmp
 
-import de.babsek.jfs25kmp.server.nativeServerMain
-
 fun main() {
-    nativeServerMain()
     val greeter = Greeting()
     println("Hello from Main")
     println("The platform specific greeting is: ${greeter.greet()}")
